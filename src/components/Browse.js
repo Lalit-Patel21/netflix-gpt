b6c6cb20aaf1bc86688680589bd370d1e6384215
Lalit-Patel21 <gurjarlalitpatel21@gmail.com>
2025-06-30 15:58:02 +0530

    maincomponent secondarycomponent videobackground videotitle movielists moviecard component done

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,10 +1,16 @@
-import React from "react";
 import Header from "./Header";
-
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
+import MainContainer from "./MainContainer";
+import SeconaryContainer from "./SeconaryContainer";
 const Browse = () => {
+  useNowPlayingMovies();
+  usePopularMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SeconaryContainer />
     </div>
   );
 };
---
+++ src/components/SeconaryContainer.js
@@ -0,0 +1,23 @@
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
+const SeconaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
+  return (
+    movies.nowPlayingMovies && (
+      <div className="bg-black">
+        <div className="-mt-54 pl-12 relative z-20 ">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies.popularMovies} />
+          <MovieList
+            title={"UpComing Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        </div>
+      </div>
+    )
+  );
+};
+
+export default SeconaryContainer;
